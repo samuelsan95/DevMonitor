e04ed3560a73ed6bcaad494da965b001a5e2ed34
Samuel Sanchez <sanchezlopezsamuel@gmail.com>
2026-03-12 17:05:14 +0100

    Initial Commit

---
+++ DevMonitor/DevMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevMonitorApp.swift
+//  DevMonitor
+//
+//  Created by Samuel Sánchez López on 12/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DevMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
